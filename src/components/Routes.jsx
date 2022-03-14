1b6dd2b7cd6053be7adc4614eadf849d9a575a26
dodoleouf <61510923+dodoleouf@users.noreply.github.com>
2022-03-14 22:24:51 +0100

    :construction: add Routes component's content

--- src/components/Routes.jsx
+++ src/components/Routes.jsx
@@ -1,9 +1,19 @@
-import React from 'react'
+import React from 'react';
+import { Router, Route, Redirect } from 'react-router-dom';
+
+import { Results } from './Results';
 
 function Routes() {
   return (
-    <div>
-        <h1>Routes</h1>
+    <div className='p-4'>
+        <Router>
+          <Route exact path="/">
+            <Redirect to="/search" />
+          </Route>
+          <Route path={['/search', '/images', '/news', '/videos']}>
+            <Results />
+          </Route>
+        </Router>
     </div>
   )
 }
