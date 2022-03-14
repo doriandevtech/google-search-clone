2f81debfe4b9cd6dbcb213362cd08811d9967edc
dodoleouf <61510923+dodoleouf@users.noreply.github.com>
2022-03-14 22:21:46 +0100

    :construction: Theme switch button

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,7 +1,9 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 
-function Navbar() {
+import { Search } from './Search';
+
+export const Navbar = ({ darkTheme, setDarkTheme }) => {
   return (
     <div className='p-5 pb-0 flex flex-wrap sm:justify-between justify-center items-center border-b dark:border-gray-700 border-gray-200'>
         <div className='flex justify-between items-center space-x-5 w-screen'>
@@ -10,6 +12,10 @@ function Navbar() {
                     Google 🔎
                 </p>
             </Link>
+            <button type='button' onClick={() => setDarkTheme(!darkTheme)} className='text-xl dark:text-gray-900 bg-white rounded-full px-2 py-1 hover:shadow-lg'>
+              {darkTheme ? 'Light 💡' : 'Dark 🌙'}
+            </button>
+            <Search />
         </div>
     </div>
   )
